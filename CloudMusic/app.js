edf5ae7a16097e19d14a3c5cd78849fc26f9eb62
YingMy <Yingmy@163.com>
2018-12-28 20:58:02 +0800

    11111

--- CloudMusic/app.js
+++ CloudMusic/app.js
@@ -10,11 +10,14 @@ App({
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log(res.code);
+        
       }
     })
     // 获取用户信息
     wx.getSetting({
       success: res => {
+        console.log(res);
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
